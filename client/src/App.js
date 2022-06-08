b26d0d81f4221fee364b4d487b95c78b302e432d
Justyn Subrai <justynskylarsubrai@gmail.com>
2022-06-07 19:34:58 -0700

    set up pages and routes on App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import Contact from './pages/Contact/Contact';
+import About from './pages/About/About';
 
 function App() {
   return (
-    <div className="App">
-      hello world - b
-    </div>
+    <Router className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ client/src/pages/Contact/Contact.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Contact() {
+  return (
+    <div>
+      welcome to contact
+    </div>
+  )
+}
+
+export default Contact
