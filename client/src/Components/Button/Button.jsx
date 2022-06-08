2fcc5026a6fd67fc6d4a52ce2c62bf29df903045
Carter <carter.endsley@gmail.com>
2022-06-07 20:59:40 -0600

    fixed sass and created some component files

---
+++ client/src/Components/Button/Button.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Button.scss'
+
+
+
+function Button() {
+  return (
+    <div className="button">
+      <h1>1 </h1>
+      <h2>2 </h2>
+    </div>
+  );
+}
+
+export default Button;
---
+++ client/src/Components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.scss'
+
+
+
+function Header() {
+  return (
+    <div className="headerf">
+      
+    </div>
+  );
+}
+
+export default Header;
