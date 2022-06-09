b370c2ea1902271e1d205eb2f2672275eec602e3
Carter <carter.endsley@gmail.com>
2022-06-08 18:39:10 -0600

    fixed styling on the header

--- client/src/pages/Global/Global.jsx
+++ client/src/pages/Global/Global.jsx
@@ -7,9 +7,12 @@ function Global() {
     <>
       <div className='nav-container'>
         <nav className='nav'>
-          <h1 className='title'>Smite Owl</h1>
+          <div className="logo-container">
+            <a href='/'><img className='logo' src='images/smitelogo.png'/></a>
+            <h1 className='title'>Smite Owl</h1>
+          </div>
           <ul className='nav-links'>
-            <li><a href='/'><img className='logo' src='images/smitelogo.png'/></a></li>
+            <li></li>
             <li><Link className='linkTags' to='/gods'>Gods</Link></li>
             <li><Link className='linkTags' to='/tier'>Tier List</Link></li>
             <li><Link className='linkTags' to='/builds'>Builds</Link></li>
